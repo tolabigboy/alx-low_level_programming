1db123586c37248deb5a1b734bdfdf3f69e3d9b8
tolabigboy <babatundetola@gmail.com>
2022-09-09 12:00:37 -0700

    My c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a'; alpha <= 'z'; alph++)
+		putchar(alph);
+	for (alph = 'A'; alph <= 'Z'; alpa++)
+		putchar(alph);
+	putchar('\n')
+
+	return (0);
+}
