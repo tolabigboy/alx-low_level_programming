a880b3523026b34a4d73be6e9834e971d18d8f4e
tolabigboy <babatundetola@gmail.com>
2022-09-09 12:22:31 -0700

    My c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	char alph, q, e;
+
+	q = 'q';
+	e = 'e';
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		if (alph != e && alph != q)
+			putchar(alph)
+
+	}
+	putchar('\n')
+
+	return (0);
+}
