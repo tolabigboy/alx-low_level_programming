129aed3448afb5c4832b15a9c65ffc68559b9763
tolabigboy <babatundetola@gmail.com>
2022-09-09 11:16:56 -0700

    My c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+		putchar(alph);
+
+	putchar('\n');
+
+	return (0);
+}
