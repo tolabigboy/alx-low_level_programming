95b7e05324c2fe8df55d15755f6a8b1dbfba0556
tolabigboy <babatundetola@gmail.com>
2022-09-09 20:05:27 -0700

    My c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,7 +11,7 @@ int main(void)
 	int num;
 	char alph;
 
-	for (num = '0'; num < '16'; num++)
+	for (num = '0'; num <= '9'; num++)
 		putchar(num);
 	for (alph = 'a'; alph <= 'f'; alph++)
 		putchar(alph);
