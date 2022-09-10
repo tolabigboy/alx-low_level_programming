a03b402f2e2ff59595c4b6e4b185e235dff30779
tolabigboy <babatundetola@gmail.com>
2022-09-09 19:46:18 -0700

    My c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.lib>
+#include <time.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	char alph;
+
+	for (alph = 'z'; alph >= 'a'; alph--)
+		putchar(alph);
+
+	putchar('\n');
+
+	return (0);
+}
