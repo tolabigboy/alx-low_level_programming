29bf98f71fae36987466867c39382e24ec855dd2
tolabigboy <babatundetola@gmail.com>
2022-09-13 11:15:08 -0700

    My c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - check code
+*
+* Return: Always 0.
+*/
+void print_alphabet_x10(void)
+{
+	int letter;
+
+	for (letter = 0; letter <= 9; letter++)
+	{
+		for (let = 'a'; let <= 'z'; let++)
+			_putchar(let);
+	}
+	_putchar('\n');
+}
