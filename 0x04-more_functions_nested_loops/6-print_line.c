8c60a9dbc15f8f386bfc4ac5e9f31798037ede94
tolabigboy <babatundetola@gmail.com>
2022-09-15 10:56:47 -0700

    My c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_line - a function that draws line
+ * @n: variable tested
+ * Return: Always 0 (success)
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 1; i <= 'n'; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
