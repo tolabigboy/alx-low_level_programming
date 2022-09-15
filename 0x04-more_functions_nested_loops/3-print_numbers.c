75b2d84e1bd169ece39c7eec2917fcd74eaf112a
tolabigboy <babatundetola@gmail.com>
2022-09-15 10:01:16 -0700

    My c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_numbers - a function that prints number
+ *
+ * Return: Always 0 (success)
+ */
+void print_numbers(void)
+{
+	char i;
+
+	for (i = 0; i <= 9; i++)
+		_putchar(i);
+
+	_putchar('\n');
+}
+
